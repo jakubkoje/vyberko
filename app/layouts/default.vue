2a96c79c61865cb855765fa1de313bc259f6bbfa
Jakub <jakubkoje@gmail.com>
2025-10-11 17:18:38 +0200

    fix

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,73 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+const toast = useToast()
+
+const open = ref(false)
+
+const links = [[{
+  label: 'Domov',
+  icon: 'i-lucide-house',
+  to: '/admin',
+  onSelect: () => {
+    open.value = false
+  },
+}, {
+  label: 'Testy',
+  icon: 'i-lucide-file-text',
+  to: '/admin/surveys',
+  onSelect: () => {
+    open.value = false
+  },
+}, {
+  label: 'Výberové konania',
+  icon: 'i-lucide-briefcase',
+  to: '/admin/procedures',
+  onSelect: () => {
+    open.value = false
+  },
+}],
+] satisfies NavigationMenuItem[][]
+
+const groups = computed(() => [{
+  id: 'links',
+  label: 'Prejsť na',
+  items: links.flat(),
+},
+])
+
+onMounted(async () => {
+  const cookie = useCookie('cookie-consent')
+  if (cookie.value === 'accepted') {
+    return
+  }
+
+  toast.add({
+    title: 'Používame cookies na zlepšenie vášho zážitku na našej webovej stránke.',
+    duration: 0,
+    close: false,
+    actions: [{
+      label: 'Prijať',
+      color: 'neutral',
+      variant: 'outline',
+      onClick: () => {
+        cookie.value = 'accepted'
+      },
+    }, {
+      label: 'Odmietnuť',
+      color: 'neutral',
+      variant: 'ghost',
+    }],
+  })
+})
+</script>
+
 <template>
-  <div>
-    <UHeader title="Nuxt UI" />
-    <UContainer>
-      <slot />
-    </UContainer>
-    <UFooter>
-      <p class="text-muted text-sm">
-        Copyright © {{ new Date().getFullYear() }}
-      </p>
-    </UFooter>
-  </div>
+  <UDashboardGroup
+    unit="rem"
+    class="static"
+  >
+    <slot />
+  </UDashboardGroup>
 </template>
